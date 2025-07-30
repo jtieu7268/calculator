90e5d8b4bb976889487f5c2a40450caa00a2d60f
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-07-30 13:40:03 -0400

    Implement basic math operator functions

---
+++ calculator.js
@@ -0,0 +1,32 @@
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+console.log(add(0,0));
+console.log(add(1, 16));
+console.log(add(-3,4));
+
+console.log(subtract(0,0));
+console.log(subtract(1, 16));
+console.log(subtract(-3,4));
+
+console.log(multiply(0,4));
+console.log(multiply(39, 45));
+console.log(multiply(-5,4));
+console.log(multiply(-5,-4));
+
+console.log(divide(1, 4));
+console.log(divide(43, 7));
+console.log(divide(500,-100));
